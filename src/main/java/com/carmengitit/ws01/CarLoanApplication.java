84b34e398bfb304436473467a8115f6052f646b1
Carmen W <Carmenwhitton@gmail.com>
2023-05-25 14:46:15 -0400

    chore: update ui

--- src/main/java/com/carmengitit/ws01/CarLoanApplication.java
+++ src/main/java/com/carmengitit/ws01/CarLoanApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CarLoanApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(CarLoanApplication.class.getResource("car-loan-input.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Car Loan Calculator");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/carmengitit/ws01/LoanInputController.java
+++ src/main/java/com/carmengitit/ws01/LoanInputController.java
@@ -1,14 +1,89 @@
 package com.carmengitit.ws01;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
-public class HelloController {
+import java.text.NumberFormat;
+
+import static java.lang.Math.pow;
+
+public class LoanInputController {
     @FXML
-    private Label welcomeText;
+    private TextField typeText;
+    @FXML
+    private TextField ageText;
+    @FXML
+    private TextField priceText;
+    @FXML
+    private TextField downPaymentText;
+    @FXML
+    private TextField interestRateText;
+    @FXML
+    private Slider paymentPeriodSlider;
+    @FXML
+    private TextField paymentFrequencyText;
+    @FXML
+    private TextField calculateText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onCalculateButtonClick() {
+        double payment = 0;
+        double price = 0;
+        double down = 0;
+        double interest = 0;
+        int months = 0;
+        int numPayments = 0;
+        String frequency = "";
+
+        try {
+            price = Double.parseDouble(priceText.getText());
+            down = Double.parseDouble(downPaymentText.getText());
+            interest = Double.parseDouble(interestRateText.getText()) / 100;    // Convert to decimal
+            months = (int) paymentPeriodSlider.getValue();
+            frequency = paymentFrequencyText.getText();
+
+            if (frequency.equalsIgnoreCase("weekly")) {
+                numPayments = months * 4;
+                interest /= (12 * 4);
+            } else if (frequency.equalsIgnoreCase("bi-weekly") || frequency.equalsIgnoreCase("biweekly")) {
+                numPayments = months * 2;
+                interest /= (12 * 2);
+            } else if (frequency.equalsIgnoreCase("monthly")) {
+                numPayments = months;
+                interest /= (12);
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error!");
+                alert.setHeaderText("Invalid Payment Frequency");
+                alert.setContentText("Please enter a valid payment frequency: weekly, biweekly, monthly.");
+                alert.showAndWait();
+            }
+
+            price -= down;
+            payment = (interest * price) / (1 - (pow(1 + interest, (-1) * numPayments)));
+
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+            String strPayment = formatter.format(payment);
+            calculateText.setText(strPayment);
+        } catch(Exception e) {
+            System.out.println(e);
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText("Invalid Inputs");
+            alert.setContentText("Please enter your information correctly and try again.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    protected void onClearButtonClick() {
+        typeText.setText("");
+        ageText.setText("");
+        priceText.setText("");
+        downPaymentText.setText("");
+        interestRateText.setText("");
+        paymentPeriodSlider.setValue(12);
+        paymentFrequencyText.setText("");
+        calculateText.setText("");
     }
 }
